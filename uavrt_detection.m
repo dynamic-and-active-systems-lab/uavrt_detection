1a460d3fbfda7932256f5d8aa40f1ba970fa351b
Don Gagne <dongagne@outlook.com>
2022-12-30 11:23:45 -0800

    update

--- uavrt_detection.m
+++ uavrt_detection.m
@@ -3,7 +3,7 @@ function [] = uavrt_detection()
 %   Detailed explanation goes here
 
  %#codegen
-configPath = "detector/config/detectorConfig.txt"; %Must exist in the same directory as the execution of this executable
+configPath = "/home/pi/repos/uavrt_detection/detector/config/detectorConfig.txt"; %Must exist in the same directory as the execution of this executable
 
 Config =  DetectorConfig(); %Build empty config object
 updateconfig()              %Update (fill) the configuration
